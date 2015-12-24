cd6e109b11529ff1a274c7c3d8af32351015b933
Raicu Vlad <raicuvladf@gmail.com>
2015-12-24 14:17:33 +0200

    [IMP] Interaction module

--- plugins/interact/plugin.py
+++ plugins/interact/plugin.py
@@ -15,14 +15,14 @@ class InteractPlugin(object):
 		nick, ident, vhost = user.group(1), user.group(2), user.group(3)
 		if command(['hello']):
 			if time.strftime("%H:%M:%S") < time.strftime("12:00:00"):
-				message = random.choice(morning_greetings)
+				greetings.extend(morning_greetings)
 			elif time.strftime("%H:%M:%S") <= time.strftime("18:00:00"):
-				message = random.choice(day_greetings)
+				greetings.extend(day_greetings)
 			else:
-				message = random.choice(night_greetings)
-			
-			greet = random.choice(greetings)
-			cardinal.sendMsg(channel, "%s %s" % (greet, user.group(1)))
+				greetings.extend(night_greetings)
+
+			greeting_message = "%s %s" % (random.choice(greetings), nick)
+			cardinal.sendMsg(channel, greeting_message)
 
 
 
