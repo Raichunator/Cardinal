672273d7b1e823e58ad6ec031aeeca5f85e1c205
Raicu Vlad <raicuvladf@gmail.com>
2015-12-30 13:40:27 +0200

    [WIP] Web server plugin

--- plugins/server/plugin.py
+++ plugins/server/plugin.py
@@ -6,13 +6,30 @@ from twisted.web.resource import Resource
 import json
 
 
-class Simple(resource.Resource):
+class Github2Irc(object):
+    def __init__(self, cardinal):
+        """Register our event"""
+        cardinal.event_manager.register('webserver.github_webhook', 1)
+        self.callback_id = cardinal.event_manager.register_callback(
+          'webserver.github_webhook', self.send_message)
+
+    def send_message(self, cardinal, user, channel):
+        cardinal.sendMsg(channel, "Am primit!")
+
+    def close(self, cardinal):
+        "Remove the event"""
+        cardinal.event_manager.remove('webserver.github_webhook')
+
+
+class Webserver(resource.Resource):
   isLeaf = True
   def render_GET(self, request):
     return "<html>Hello, world!</html>"
 
-  def render_POST(self, request, user, channel, cardinal, msg):
+  def render_POST(self, request):
     data = json.loads(request.content.read())
+    hook_name = data['hook']['name']
+    cardinal.event_manager.fire('webserver.github_webhook')
     import ipdb;ipdb.set_trace()
     return ""
 
@@ -21,5 +38,4 @@ site = server.Site(Simple())
 reactor.listenTCP(8973, site)
 
 def setup():
-    return Simple()
-
+    return Webserver()
