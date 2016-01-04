b173d60605ca219649fcf7cbb1a4068c7f46ae05
raichunator <raicuvladf@gmail.com>
2016-01-04 14:29:28 +0100

    [ADD] Added issue tracker for github

--- plugins/web_server/plugin.py
+++ plugins/web_server/plugin.py
@@ -12,21 +12,22 @@ class Webserver(resource.Resource):
     def __init__(self, cardinal, config):
         self.cardinal = cardinal
 	self.config = config
+	self.allowed_actions = ['created', 'opened']
+
 
     def render_GET(self, request):
         return "<html>Hello, world!</html>"
 
     def render_POST(self, request, **kwargs):
         data = json.loads(request.content.read())
-        hook_name = data['hook']['name']
-        issue_id = data['issue']['id']
-        if 'id' in issue_id:
-            self.cardinal.sendMsg(self.config['channel'].encode('utf8'), str(issue_id))
-            return "<html><body>Issue Number: </body></html>"
+	import pdb; pdb.set_trace()
+        if 'action' in data:
+	    if data['action'] in self.allowed_actions:
+	        if 'issue' in data: 
+ 		    message = ("New issue: %s" % data['issue']['html_url']).encode('utf8')
+           	    self.cardinal.sendMsg(self.config['channel'].encode('utf8'), message)
 
-	self.cardinal.sendMsg(
-	    self.config['channel'].encode('utf8'), str(hook_name))
-        return "<html><body>Payload received</body></html>"
+        return "<html><body>Issue comment recieved received</body></html>"
 
 def setup(cardinal, config):
     site = server.Site(Webserver(cardinal, config))
